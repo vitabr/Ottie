705c80f3863940380416c03342eab8eb3da9f557
Vitaliy A <vitabr@gmail.com>
2019-03-24 19:36:20 +0200

    Vito: Working on MLKit simplified interface

---
+++ app/src/main/java/restforyou/com/tofsologia/MLKit.java
@@ -0,0 +1,83 @@
+package restforyou.com.tofsologia;
+
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModelSource;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+/**
+ * MLKit  provide simplified interface to Machine Learning from Firebase
+ * Important: Call init() on application start to ensure correct module initialization
+ */
+public class MLKit {
+
+    public interface OnRecognizeListener{
+        void onSuccess(FirebaseVisionText texts);
+        void onFailure(@NonNull Exception e);
+    }
+
+    private static MLKit _instance = new MLKit();
+    private static final String LOCAL_MODEL_ASSET = "mobilenet_v1_1.0_224_quant.tflite";
+    private static FirebaseModelInterpreter mInterpreter;
+
+    private MLKit(){
+
+        try {
+            FirebaseLocalModelSource localSource =
+                    new FirebaseLocalModelSource.Builder("asset")
+                            .setAssetFilePath(LOCAL_MODEL_ASSET).build();
+            FirebaseModelManager manager = FirebaseModelManager.getInstance();
+            manager.registerLocalModelSource(localSource);
+            FirebaseModelOptions modelOptions =
+                    new FirebaseModelOptions.Builder()
+                            .setLocalModelName("asset")
+                            .build();
+            mInterpreter = FirebaseModelInterpreter.getInstance(modelOptions);
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void init(){
+
+        if(_instance == null){
+            _instance = new MLKit();
+        }
+
+    }
+
+    public static void recognize(Bitmap bitmap, final OnRecognizeListener listener){
+
+        init();
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                               listener.onSuccess(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                listener.onFailure(e);
+                            }
+                        });
+    }
+
+}
